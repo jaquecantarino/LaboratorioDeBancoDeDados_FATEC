f1c5c288ead108c0669032f5df840efb43bdd11c
jaquecantarino <jaquelinecantarino@outlook.com>
2021-08-17 21:42:43 -0300

    Exercicio Aula 3

---
+++ lbd03.sql
@@ -0,0 +1,16 @@
+/* create database vendedor; */
+
+use vendedor;
+/* create table vendedor (
+codigo int auto_increment,
+vendedor varchar (30) not null,
+telefone varchar(9) not null,
+comissao int(3),
+primary key (codigo)
+); */
+
+-- alter table vendedor add email varchar(50);
+
+-- alter table vendedor drop column email;
+
+drop table vendedor;
